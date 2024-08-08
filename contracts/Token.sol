a665e61992e4a338145f2a7c9fb9ab84472176d1
dibania200 <146622460+dibania200@users.noreply.github.com>
2024-08-08 09:52:29 +0000

    feat: initiated the project

---
+++ contracts/Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor()ERC20("fae7836c9b1295817a1b72b47c7d88181653303b3aeb32a18053d13bdf32e3d4","sin"){} 
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function burn100tokens() public{
+        _burn(msg.sender, 100*10**18);
+    }
+}
